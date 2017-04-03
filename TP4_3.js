8bcfc1bf0b3cc97255aa4202f29cb2d41a9fbc1b
Emyrek <emeric.mutzig@gmail.com>
2017-04-03 15:59:10 +0200

    ajout réception entrées et affichage
    problème: ça ne s'affiche que pendant une seconde, puis ça disparait

---
+++ TP4_3.js
@@ -0,0 +1,8 @@
+function add()
+{
+	document.getElementById("test").innerHTML=document.getElementById("prenom").value;
+	document.getElementById("test").innerHTML=document.getElementById("nom").value;
+	document.getElementById("test").innerHTML=document.getElementById("numero").value;
+	document.getElementById("test").innerHTML=document.getElementById("section").value;
+}
+
