3ba84d47432cc744a577b8f8ed857a4f1d78225a
Emyrek <emeric.mutzig@gmail.com>
2017-04-05 14:19:44 +0200

    bon là ça marche plus

--- TP4_4.js
+++ TP4_4.js
@@ -3,17 +3,29 @@ function add()
 //	document.getElementById("test").innerHTML=document.getElementById("prenom").value + "\n" + document.getElementById("nom").value + "\n" + document.getElementById("numero").value + "\n" + document.getElementById("section").value;
 
 
-	var paragraphe=document.createElement("p");
+	var paragraphe1=document.createElement("p");
 	var node1=document.createTextNode(document.getElementById("prenom").value)
-	paragraphe.appendChild(node1);
+	paragraphe1.appendChild(node1);
+	var element=document.getElementById("modifDOM");
+	element.appendChild(paragraphe1);
+
+	var paragraphe2=document.createElement("p");
 	var node2=document.createTextNode(document.getElementById("nom").value)
 	paragraphe.appendChild(node2);
+	element=document.getElementById("modifDOM");
+	element.appendChild(paragraphe2);
+
+	var paragraphe3=document.createElement("p");
 	var node3=document.createTextNode(document.getElementById("numero").value)
 	paragraphe.appendChild(node3);
+	element=document.getElementById("modifDOM");
+	element.appendChild(paragraphe3);
+
+	var paragraphe4=document.createElement("p");
 	var node4=document.createTextNode(document.getElementById("section").value)
 	paragraphe.appendChild(node4);
-	var element=document.getElementById("modifDOM");
-	element.appendChild(paragraphe);
+	element=document.getElementById("modifDOM");
+	element.appendChild(paragraphe4);
 
 }
 
@@ -26,3 +38,4 @@ function compte()
 add();
 
 compte();
+
